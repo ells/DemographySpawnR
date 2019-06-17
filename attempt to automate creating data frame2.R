fb31869231a76d09f8e6b6af40e04d6c8ee4ee64
Ishaan Dave <idave@uga.edu>
2019-06-17 13:43:21 -0400

    updated script

--- attempt to automate creating data frame2.R
+++ attempt to automate creating data frame2.R
@@ -1,5 +1,6 @@
+library(readxl)
 dat = read.csv("/Users/ishaandave/Desktop/CDC-Leidos/Data/Pretend/mess around.csv")
-# dat = fread("~")
+dat = read_xlsx("/Users/ishaandave/Desktop/CDC-Leidos/Data/Pretend/pretend demographics.xlsx")
 
 names(dat)<- tolower(names(dat))
 dat$sex = sample(c("M", "F", "O"), 30, prob = c(0.333, 0.333, 0.333), replace = T)
@@ -7,9 +8,9 @@ dat$education = sample(c(1:8), 30, T)
 
 gender = dat[, grepl("gender|sex", names(dat))]; gender
 ## pick whatever 
-age = dat[, grepl("age", names(dat))]; age
+ageVALUE = dat[, grepl("age", names(dat))]; ageVALUE
 
-race = dat[, grepl("race", names(dat))]; race
+raceVALUE = dat[, grepl("race", names(dat))]; raceVALUE
 
 edu = dat[, grepl("edu", names(dat))]; edu
 hisp = dat[, grepl("hisp", names(dat))]; hisp 
@@ -19,24 +20,18 @@ hisp = dat[, grepl("hisp", names(dat))]; hisp
 n = 1000
 set.seed(1234)
 patients <- tibble( 
-  `Gender` = sample(c(as.character(as.data.frame(table(gender))$gender)), n, TRUE, 
-                 prob = c(as.data.frame(table(gender))$Freq)
-                   ),
-  `Race/Ethnicity` = sample(c(as.character(as.data.frame(table(raceVALUE))$raceVALUE)), n, TRUE,
-                            prob = c(as.data.frame(table(raceVALUE))$Freq)
-                           ),
-  `Age` = round(rnorm(n, mean(ageVALUE), sd = sd(ageVALUE)
-                )),
+  `Gender`                = sample(c(as.character(as.data.frame(table(gender))$gender)), n, TRUE, 
+                             prob = c(as.data.frame(table(gender))$Freq)
+                                  ),
+  `Race/Ethnicity`        = sample(c(as.character(as.data.frame(table(raceVALUE))$raceVALUE)), n, TRUE,
+                              prob = c(as.data.frame(table(raceVALUE))$Freq)
+                                  ),
+  `Age`                   = round(rnorm(n, mean(ageVALUE), sd = sqrt(var(ageVALUE)))
+                                  ),
   `Education Categorical` = sample(c(as.character(as.data.frame(table(edu))$edu)), n, TRUE,
                                    prob = c(as.data.frame(table(edu))$Freq)
                                   ),
-  `Education Continuous` = round(rnorm(n, mean(edu), sd = sd(edu)
-                                 )),
+  `Education Continuous`  = round(rnorm(n, mean(edu), sd = sqrt(var(edu)))
+                                 )
                    )
-  `State` = sample(c(state.name), n, TRUE,
-                   prob = c(rep(.02, 50))
-  )
-)
-
-
-
+  
